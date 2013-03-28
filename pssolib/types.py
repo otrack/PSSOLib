be13f74e02e4c18ac847b803e9495095279e4b62
otrack <0track@gmail.com>
2013-03-28 15:52:29 +0100

    First add

---
+++ pssolib/types.py
@@ -0,0 +1,149 @@
+import uuid
+import nanotime
+
+from pssolib.utils import *
+
+class Splitter():
+
+    def __init__(self,key):
+        self.key = key
+        self.pid = get_thread_ident()
+        self.SPLITTER = Config.get().SPLITTER
+
+    def split(self):
+        self.SPLITTER.insert(self.key,{'x':self.pid})
+        try:
+            y = self.SPLITTER.get(self.key,columns=['y'])
+            if y!=self.pid:
+                return False
+        except NotFoundException:
+            pass
+
+        self.SPLITTER.insert(self.key,{'y':self.pid})
+        x = self.SPLITTER.get(self.key,columns=['x'])
+        if x['x']!=self.pid:
+            return False
+        return True
+
+class Consensus():
+
+    def __init__(self,key):
+        self.key = key
+        self.CONSENSUS = Config.get().CONSENSUS
+
+    def propose(self,u):
+        s = Splitter(self.key)
+        isWin = s.split()
+        if isWin==True :
+            # print "I win "+str(s.key)
+            self.CONSENSUS.insert(self.key,{'v':u})
+            # print "Value added"
+            try:
+                self.CONSENSUS.get(self.key,columns=['b'])
+                # print "Conflict seen"
+            except NotFoundException:
+                # print "No conflict"
+                return u
+        else :
+            # print "I lose "+str(s.key)
+            self.CONSENSUS.insert(self.key,{'b':1})
+            # print "Conflict added"
+            try:
+                u = self.CONSENSUS.get(self.key,columns=['v'])['v']
+                # print "Value seen"
+            except NotFoundException:
+                pass
+                # print "No Value"
+        nested=Consensus(uuid_incr(self.key))
+        return nested.propose(u)
+
+class Cas():
+
+    def __init__(self,key):
+        self.key = key
+        self.CAS = Config.get().CAS
+
+    def compareandswap(self,u,v):
+        try:
+            #FIXME
+            # columns = self.CAS.get(self.key, column_count=100000)
+            # largest=max(columns.iterkeys())
+            # if u==columns[largest]:
+            c = self.CAS.get(self.key, column_count=1)
+            (lkey,lval) = c.popitem()
+            if u == lval:
+                return self.doConsensusAndWrite(v,lkey+1)
+            return False
+        except NotFoundException:
+            pass
+        if u == None:
+            return self.doConsensusAndWrite(v,1)
+        else:
+            return False
+
+    def get(self):
+        try:
+            return self.CAS.get(self.key, column_count=1).popitem()[1]
+        except:
+            return None
+            
+    def doConsensusAndWrite(self,v,l):
+        k=uuid.uuid5(self.key,str(l))
+        c=Consensus(k)
+        # incorrect, we must propose also our ID !!
+        proposed = v
+        # print "START  "+str(l)
+        decided = c.propose(proposed)
+        # print "OVER "+str(l)+" : "+str(decided) +" "+str(proposed)
+        self.CAS.insert(self.key,{l:decided})
+        if decided == proposed:
+            return True
+        return False
+
+class Spinlock():
+
+    def __init__(self,lockid):
+        self.pid = get_thread_ident()
+        self.lockid = lockid
+        self.cas = Cas(self.lockid)
+        self.cas.compareandswap(None,str(0))
+        
+    def lock(self):
+        while self.cas.compareandswap(str(0),str(get_thread_ident())) != True:
+            pass
+        # print str(nanotime.now())+" LOCKED" + str(get_thread_ident())
+        
+    def unlock(self):
+         r = self.cas.compareandswap(str(get_thread_ident()),str(0))
+         assert r == True
+         # print str(nanotime.now())+" UNLOCKED" + str(get_thread_ident())
+
+class Map():
+
+    def __init__(self,key):
+        self.MAP = Config.get().MAP
+        self.key = key
+        
+    def get(self,index):        
+        return self.MAP.get(self.key,columns=[index])
+    
+    def put(self,index,value):
+        self.MAP.insert(self.key,{index:value})
+
+    # linearizable call cause every column is linearizable 
+    def toOrderedDict(self): 
+        over = False
+        previous = None
+        while over != True:
+            current = self.MAP.get(self,column_count=cass_max_col)
+            over = (previous == current)
+            previous = current
+        return current
+
+    def keyset(self):
+        return self.toOrderedDict().keyset()
+
+    def valueset(self):
+        return self.toOrderedDict().valueset()
+
+
---
+++ pssolib/utils.py
@@ -0,0 +1,217 @@
+####################################
+# Cassandra
+####################################
+
+cass_max_col = 2147483647
+
+
+####################################
+# Config 
+####################################
+
+from pycassa.pool import ConnectionPool
+from pycassa.columnfamily import ColumnFamily
+from pycassa.cassandra.ttypes import NotFoundException
+from pycassa.types import *
+from pycassa.system_manager import *
+from pycassa.cassandra.ttypes import InvalidRequestException, ConsistencyLevel
+
+class Config():
+
+    _instance = None
+
+    @classmethod
+    def get(cls):
+        return cls._instance
+
+    @classmethod
+    def create(cls,parser,init):
+        cls._instance = cls(parser,init)
+
+    def __init__(self,parser,init):
+
+        # 1 - read the config parser
+
+        self.SYSM = SystemManager(parser.get("main","servers"))
+        self.KEYSPACE = parser.get("main","keyspace")
+        self.SERVERS = parser.get("main","servers").split(",")
+
+        # 2 - get or create the keyspace and the column families for each object
+
+        if init:
+            try:
+                self.SYSM.drop_keyspace(self.KEYSPACE)
+            except:
+                pass
+            self.SYSM.create_keyspace(self.KEYSPACE, SIMPLE_STRATEGY, {'replication_factor': '1'})
+            self.SYSM.create_column_family(self. KEYSPACE, 'splitter', \
+                                               super=False, \
+                                               read_consistency_level = ConsistencyLevel.ALL, \
+                                               write_consistency_level = ConsistencyLevel.QUORUM)
+            self.SYSM.create_column_family(self. KEYSPACE, 'consensus', \
+                                               super=False, \
+                                               read_consistency_level = ConsistencyLevel.ALL, \
+                                               write_consistency_level = ConsistencyLevel.QUORUM)
+            self.SYSM.create_column_family(self. KEYSPACE, 'cas', \
+                                               super=False, \
+                                               comparator_type=IntegerType(reversed=True), \
+                                               read_consistency_level = ConsistencyLevel.ALL, \
+                                               write_consistency_level = ConsistencyLevel.QUORUM)
+
+        self.POOL = ConnectionPool(self.KEYSPACE, server_list=self.SERVERS) 
+
+        self.SPLITTER = ColumnFamily(self.POOL, 'splitter')
+        self.SPLITTER.key_validation_class = LexicalUUIDType()
+        self.SPLITTER.column_name_class = AsciiType()
+        self.SPLITTER.column_validators['x'] = IntegerType() # PID
+        self.SPLITTER.column_validators['y'] = IntegerType() # PID
+
+        self.CONSENSUS = ColumnFamily(self.POOL, 'consensus')
+        self.CONSENSUS.key_validation_class = LexicalUUIDType()
+        self.CONSENSUS.column_name_class = AsciiType() 
+        self.CONSENSUS.column_validators['b'] = IntegerType() # Conflict flag
+        # CONSENSUS.column_validators['v'] = BytesType() # Consensus value
+
+        self.CAS = ColumnFamily(self.POOL, 'cas')
+        self.CAS.key_validation_class = LexicalUUIDType()
+        self.CAS.column_name_class = IntegerType()
+        # self.CAS.column_validators['l'] = CompositeType(LongType(), AsciiType()) # CAS values
+
+#############################
+# UUID 
+##############################
+
+import binascii, string
+import uuid
+
+# Use snowflacke for generating UUID ?
+
+# This function takes as argument an hex and increments it modulo its lenght.
+# The result is a pair where the first element is the incremented element, 
+# and the second one is the carry.
+def hex_incr(h):
+    l=len(h)
+    if h=="f"*l:
+        return [1,"0"*l]
+    else:
+        # FIXME find a cleaner solution
+        return [0,string.replace(string.replace(hex(int(h,16)+1),"0x",""),"L","").zfill(l)]
+
+# This function takes as argument a UUID (v1) and increment the nanoseconds 
+# value by 1 . For instance, 550e8400-e29b-41d4-a716-446655440000
+# beciles 550e840ef-e29b-41d4-a717-446655440000.
+def uuid_incr(u):
+    r=str(u)
+    # FIXME find a cleaner solution 
+    #       _and_ do not change the value of the M^th byte.
+    once=True
+    for p in str(u).split("-")[::-1]:
+        if once :
+            once=False
+            continue
+        q=hex_incr(p)
+        r=string.replace(r,p,q[1])
+        if q[0]==0:
+            return uuid.UUID(r)
+    return uuid.UUID(r)
+
+# This function adds i to uuid u
+def uuid_add(u,i):
+    for k in range(1,i+1):
+        u=uuid_incr(u)
+    return u
+
+
+#############################
+# Threads
+##############################
+
+import thread
+
+# FIXME return a system-wide unique id
+#       use injjection from N*N to N
+def get_thread_ident():
+    return thread.get_ident()
+
+
+#############################
+# Files
+##############################
+
+import os,fuse
+
+class Filestats(fuse.Stat):
+    def __init__(self):
+        self.st_mode = 0
+        self.st_ino = 0
+        self.st_dev = 0
+        self.st_nlink = 0
+        self.st_uid = 0
+        self.st_gid = 0
+        self.st_size = 0
+        self.st_atime = 0
+        self.st_mtime = 0
+        self.st_ctime = 0
+
+def check_filename_len(path):
+    for p in path.split('/'):
+        if len(p) > 255:
+            raise NameTooLongException
+
+def user_in_group(uid,gid):    
+    try:
+        if pwd.getpwuid(uid).pw_gid == gid:
+            return True
+    except KeyError:
+        pass
+
+    try:
+        for user in grp.getgrgid(gid).gr_mem:
+            if uid == pwd.getpwnam(user).pw_uid:
+                return True
+    except KeyError:
+        pass
+
+    return False
+    
+def get_path_components(path):
+    paths = [ path ]
+    while path != '/':
+        path = os.path.dirname(path)
+        paths.insert(0,path)
+    try:
+        end = paths.pop()
+    except IndexError:
+        end = ''
+
+    try:
+        notend = paths.pop()
+    except IndexError:
+        notend = ''
+    
+    return (end, notend, paths)
+
+def replace(s,buf,p):
+    ns = ""
+    n = len(s)
+    bn = len(buf)
+    if n <= p:
+        ns = s + ('\0' * (p-n)) + buf
+    else:
+        ns = s[0:p] + buf + s[p+bn:]
+    return
+
+class NameTooLongException(Exception):
+    pass
+
+class EnoentException(Exception):
+    pass
+
+class EnotdirException(Exception):
+    pass
+
+class EaccessException(Exception):
+    pass
+
+class EexistException(Exception):
+    pass
