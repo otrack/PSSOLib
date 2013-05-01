b3e5de740ccc3d3a82045f0b64e11672c502df4d
otrack <0track@gmail.com>
2013-05-02 01:41:09 +0200

    a

--- scripts/dist_spin.sh
+++ scripts/dist_spin.sh
@@ -4,6 +4,10 @@ bc=`which bc`
 EXP_TMP_DIR=/tmp/spinexp
 N_IT=100
 
+function absolute(){
+    awk ' { if($1>=0) { print $1} else {print $1*-1 }}'
+}
+
 # 0 - Usage 
 if [ $# -ne 3 ]
 then
@@ -20,7 +24,7 @@ fi;
 
 # 2 - Launch experiments
 
-client_min=10
+client_min=1
 client_max=10
 client_incr=1
 
@@ -28,7 +32,7 @@ in_nap_min=1
 in_nap_max=1
 in_nap_incr=1
 
-out_nap_min=0
+out_nap_min=50
 out_nap_max=50
 out_nap_incr=10
 
@@ -56,20 +60,38 @@ do
 	    wait 
 
             # 2.2 Gather results
- 	    latency=0
+
+ 	    tlat=0
 	    for i in `seq 1 ${nclients}` 
 	    do
 		tmp=`grep PID ${EXP_TMP_DIR}/$i | awk  '{print $2}'`
 		if [ -n "${tmp}" ]
 		then
-		    latency=`echo "${latency}+${tmp}"| sed 's/E/*10^/g'`
+		    tlat=`echo "${tlat}+${tmp}-${in_nap}-${out_nap}"| sed 's/E/*10^/g'`
 		else
     		    echo "Experiment is corrupted; aborting !"
     		    exit 1
 		fi;
 	    done
-	    latency=`echo "scale=2;(${latency})/${nclients}" | ${bc}`
-	    echo -e "${nclients}\t${latency}\t${in_nap}\t${out_nap}"
+	    latency=`echo "scale=2;(${tlat})/${nclients}" | ${bc}`
+
+ 	    tspread=0
+	    for i in `seq 1 ${nclients}` 
+	    do
+		tmp=`grep PID ${EXP_TMP_DIR}/$i | awk  '{print $2}'`
+		if [ -n "${tmp}" ]
+		then
+		    lat=`echo "${tmp}-${in_nap}-${out_nap}"| sed 's/E/*10^/g'`
+		    spread=`echo "scale=2;${lat}-${latency}" | ${bc}`
+		    tspread=`echo "${tspread}+${spread#-}"`
+		else
+    		    echo "Experiment is corrupted; aborting !"
+    		    exit 1
+		fi;
+	    done
+ 	    stddev=`echo "scale=2;(${tspread})/${nclients}" | ${bc}`
+
+	    echo -e "${nclients}\t${latency}\t${stddev}\t${in_nap}\t${out_nap}"
 	  
 	done
 
