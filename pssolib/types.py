c7bcc7c18c6792863bc84481e941644bdc75d5a9
otrack <0track@gmail.com>
2013-05-02 03:28:24 +0200

    a

--- pssolib/types.py
+++ pssolib/types.py
@@ -127,12 +127,15 @@ class Spinlock():
         self.cas.compareandswap(None,str(0))
         
     def lock(self):
-        # mdelay=0
+        mdelay=0
+        maxdelay=0.010
         while self.cas.compareandswap(str(0),str(get_thread_ident())) != True:
-            # if mdelay==0:
-            #     mdelay=0.001
-            # mdelay=mdelay*2 # exp backoff
-            # time.sleep(mdelay)
+            if mdelay==0:
+                mdelay=0.001
+            mdelay=mdelay+0.001 # lin backoff
+            if mdelay>maxdelay:
+                mdelay=maxdelay # with a cap
+            time.sleep(mdelay)
             pass
         print str(nanotime.now())+" LOCKED" + str(get_thread_ident())
         
