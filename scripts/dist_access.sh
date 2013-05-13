d7f67d720efd33b3e312bd664cac599ce48f6684
otrack <0track@gmail.com>
2013-05-13 23:48:37 +0200

    neant

--- scripts/dist_access.sh
+++ scripts/dist_access.sh
@@ -2,7 +2,7 @@
 
 bc=`which bc`
 EXP_TMP_DIR=/tmp/exp
-N_IT=1000
+N_IT=500
 
 function absolute(){
     awk ' { if($1>=0) { print $1} else {print $1*-1 }}'
@@ -30,8 +30,8 @@ fi
 
 # 2 - Launch experiments
 
-client_min=1
-client_max=10
+client_min=4
+client_max=4
 client_incr=1
 
 nap_min=0
@@ -41,8 +41,8 @@ nap_incr=10
 for nclients in `seq ${client_min} ${client_incr} ${client_max}`
 do
 
-    napseq=`seq ${nap_min} ${nap_incr} ${nap_max}`
-    # napseq="1 $((${nclients}*2)) $((${nclients}*5))"
+    # napseq=`seq ${nap_min} ${nap_incr} ${nap_max}`
+    napseq="$((${nclients}*2)) $((${nclients}*5)) $((${nclients}*10))"
     for nap in ${napseq}
     do
 
