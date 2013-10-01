db165a3da101712402e39b1e620ae37fce6abe2e
otrack <0track@gmail.com>
2013-10-01 23:27:18 +0200

    [FIX] bounded implem.

--- scripts/dist_access.sh
+++ scripts/dist_access.sh
@@ -24,9 +24,9 @@ fi
 
 # 2 - Launch experiments
 
-client_min=1
-client_max=10
-client_incr=1
+client_min=10
+client_max=100
+client_incr=10
 
 nap_min=0
 nap_max=0
@@ -41,9 +41,9 @@ do
     do
 
         # 2.1 - Run an  experiment	
-	objectid=`uuidgen`
 	for i in `seq 1 ${nclients}` 
 	do
+	    objectid=`uuidgen`
 	    ./access $1 $2 $3 ${objectid} ${N_IT} ${nap} &> ${EXP_TMP_DIR}/$i &
 	done
 	wait 
