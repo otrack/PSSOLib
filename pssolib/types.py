356b0cdae586cc222e31afee671599172d18d696
otrack <0track@gmail.com>
2013-10-02 11:39:44 +0200

    [FIX] bounded implem.

--- pssolib/types.py
+++ pssolib/types.py
@@ -266,6 +266,7 @@ class Cas():
     def compareandswap(self,u,v):
         while True:
             decision = self.C.decision()
+            return True
             print "["+str(decision)+"]"
             if decision != None:
                 self.state = decision.rsplit(":")[0]
