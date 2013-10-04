1804f16515dba8ee26de8b30fcd76fbf2b3002e8
otrack <0track@gmail.com>
2013-10-04 11:22:05 +0200

    [ADD] sharded client mode

--- scripts/sharded_access.sh
+++ scripts/sharded_access.sh
@@ -20,7 +20,7 @@ trap "echo 'Caught Quit Signal'; stopExp; wait; exit 255" SIGINT SIGTERM
 let e=${#clients[@]}-1
 for i in `seq 0 $e`
 do
-    ${SSHCMD} " " ${clients[$i]} "cd ${SCRIPT_DIR} && ${SCRIPT_DIR}/dist_access.sh" $1 $2 $3&
+    ${SSHCMD} ${clients[$i]} "cd ${SCRIPT_DIR} && ${SCRIPT_DIR}/dist_access.sh" $1 $2 $3&
 done
 
 wait 
