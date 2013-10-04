384df5dc99e5712c709c50c336c79344fd7649c8
otrack <0track@gmail.com>
2013-10-04 11:17:09 +0200

    [ADD] sharded client mode

---
+++ scripts/config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SSHCMD="ssh"
+
+bc=`which bc`
+SCRIPT_DIR=/opt/PSSOLIB/scripts #/home/otrack/ALL/myWork/Implementation/PSSOLib/scripts
+EXP_TMP_DIR=/tmp/exp
+N_IT=2000
+
+client_min=50
+client_max=100
+client_incr=10
+
+nap_min=0
+nap_max=0
+nap_incr=10
--- scripts/sharded_access.sh
+++ scripts/sharded_access.sh
@@ -2,7 +2,7 @@
 
 source config.sh
 
-clients=("192.168.79.35") # "192.168.79.41" "192.168.79.42" "192.168.79.43")
+clients=("localhost") # "192.168.79.41" "192.168.79.42" "192.168.79.43")
 
 function stopExp(){
 
@@ -20,7 +20,7 @@ trap "echo 'Caught Quit Signal'; stopExp; wait; exit 255" SIGINT SIGTERM
 let e=${#clients[@]}-1
 for i in `seq 0 $e`
 do
-    ssh root@${c} ~/${SCRIPT_DIR}/dist_access &
+    ssh ${clients[$i]} "cd ${SCRIPT_DIR} && ${SCRIPT_DIR}/dist_access.sh" $1 $2 $3&
 done
 
 wait 
